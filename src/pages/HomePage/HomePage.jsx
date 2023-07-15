0abcf271a91bb1738d438d6eb51301442782165b
Turq-uoise <Turquoise_2@outlook.com>
2023-07-15 17:46:58 +0100

    Fix broken push

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,23 @@
-export default function HomePage() {
+import { useEffect, useState } from "react";
+import ThreeByThree from "../../components/ThreeByThree/ThreeByThree";
+import * as threeByThreesAPI from "../../utilities/threebythrees-api"
+import { Container, Row, Col, Button } from "react-bootstrap";
+
+export default function HomePage({showTitle, setShowTitle}) {
+  const [threeByThrees, setThreeByThrees] = useState([])
+
+  useEffect(function() {
+    async function getThreeByThrees() {
+      const threeByThrees = await threeByThreesAPI.getAll();
+      setThreeByThrees(threeByThrees);
+    }
+    getThreeByThrees();
+  }, [])
 
   return (
-    <Container>
+    <Container className="HomePage">
       <Row>
-        <Col md={9} className="HomePage">
+        <Col md={9}>
           <h1>HomePage</h1>
           {threeByThrees.map(({items, user}) => (
             <ThreeByThree items={items} user={user} showTitle={showTitle}/>
